642f61cb4aaba2d85bedda663c7fd435f4e94cd9
hologramshadow <140269871+hologramshadow@users.noreply.github.com>
2024-02-26 13:50:58 +0900

    Update idlegame.js

--- idlegame.js
+++ idlegame.js
@@ -36,12 +36,13 @@ window.onload = ()=> {
       halve();
       dCounter=0;
     } 
+  }
   var saveData = function () {
     //セーブの処理
      localStorage.setItem('coin',coin);
      localStorage.setItem('cPtick',cPtick);
      localStorage.setItem('cMax',cMax);
-    } 
+    
   }
   var halve = function(){
     //半減期ごとの処理
